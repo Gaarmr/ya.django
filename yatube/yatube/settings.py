fc021c3a8f56abc16e0abfe82097b4bd5c61b5ab
Daniil Romanshen <vdnv@bk.ru>
2022-04-26 06:13:21 +0600

    update

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -125,8 +125,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# Login
 
+# Login
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index" 
 # LOGOUT_REDIRECT_URL = "index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
